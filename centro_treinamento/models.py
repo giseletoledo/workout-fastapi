f887fd9581aab488d054b04d07fed0183a8b68d6
Gisele <gisesonia@gmail.com>
2026-01-08 21:44:06 -0300

    Add CentroTreinamento model and schema with additional fields

---
+++ centro_treinamento/models.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import List
+from atleta.models import AtletaModel
+from contrib.models import BaseModel
+
+
+class CentroTreinamentoModel(BaseModel):
+    __tablename__ = 'centro_treinamento'
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    nome: Mapped[str] = mapped_column(String(20), unique=True, nullable=False)
+    endereco: Mapped[str] = mapped_column(String(60), nullable=False)
+    proprietario: Mapped[str] = mapped_column(String(30), nullable=False)
+
+    atleta: Mapped[List["AtletaModel"]] = relationship("AtletaModel", back_populates="centro_treinamento")
