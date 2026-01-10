e680f0cea3d5d50b749db6b89f364b05387787d6
Gisele <gisesonia@gmail.com>
2026-01-10 16:25:06 -0300

    Add Atleta model and controller with dependency injection for database session

--- atleta/controller.py
+++ atleta/controller.py
@@ -1,7 +1,18 @@
 from fastapi import APIRouter
+from fastapi import Body, status
+from uuid import uuid4
+from contrib.dependencies import DatabaseDependency
 
 router = APIRouter()
 
-@router.post(path="/")
-async def post():
+@router.post(
+    '/', 
+    summary='Criar um novo atleta',
+    status_code=status.HTTP_201_CREATED,
+    response_model=AtletaOut
+)
+async def post(
+    db_session: DatabaseDependency, 
+    atleta_in: AtletaIn = Body(...)
+):
     pass
--- atleta/schemas.py
+++ atleta/schemas.py
@@ -2,7 +2,7 @@ from typing import Annotated
 
 from pydantic import Field, PositiveFloat
 
-from contrib.schemas import BaseSchema
+from contrib.schemas import BaseSchema, OutMixin
 
 class Atleta(BaseSchema):
     nome: Annotated[str, Field(description='Nome do atleta', example='Joao', max_length=50)]
@@ -11,3 +11,10 @@ class Atleta(BaseSchema):
     peso: Annotated[PositiveFloat, Field(description='Peso do atleta', example=75.5)]
     altura: Annotated[PositiveFloat, Field(description='Altura do atleta', example=1.70)]
     sexo: Annotated[str, Field(description='Sexo do atleta', example='M', max_length=1)]
+
+class AtletaIn(Atleta):
+    pass
+
+
+class AtletaOut(Atleta, OutMixin):
+    pass
